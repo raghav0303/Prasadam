f7b3a712753a673977e27be7268279700091fe15
raghav0303 <raghav03032002joshi@gmail.com>
2022-11-18 08:23:41 +0530

    mapbox map integrated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prasadam/screens/home_screen.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,7 +9,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //drawer: NavigationDrawer(),
+      body: FlutterMap(
+    options: MapOptions(
+        center: latLng.LatLng(21.1938, 81.3509),
+        zoom: 9.2,
+    ),
+    nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+            source: 'OpenStreetMap contributors',
+            onSourceTapped: null,
+        ),
+    ],
+    children: [
+        TileLayer(
+            urlTemplate: 'https://api.mapbox.com/styles/v1/raghavjoshi03/clalvihxb000514o68pjwdzby/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmFnaGF2am9zaGkwMyIsImEiOiJjbDJycW91c3cxd3dxM2Jrem1mczV3ZXRqIn0.YEIXcheELO9KHKgPjb7S8g',
+            userAgentPackageName: 'com.example.app',
+            additionalOptions: {
+              'accessToken':'pk.eyJ1IjoicmFnaGF2am9zaGkwMyIsImEiOiJjbDJycW91c3cxd3dxM2Jrem1mczV3ZXRqIn0.YEIXcheELO9KHKgPjb7S8g',
+              'id' : 'mapbox.satellite',
+            }
+        ),
+    ],
+)
     );
   }
 }
